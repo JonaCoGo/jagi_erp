addb273530b238ab204f51eb5b5917fd4cb25c8f
Jonatan Corrales <jonatancorralesgomez@gmail.com>
2026-01-15 12:05:31 -0500

    actualizacion: simplificar consultas

--- app/consultas.py
+++ app/consultas.py
@@ -1,7 +1,6 @@
 # consultas.py
 
-import pandas as pd
-from datetime import datetime, timedelta
+
 from app.services.analisis_marca_service import get_analisis_marca
 from app.services.producto_service import get_consulta_producto
 from app.services.existencias_service import get_existencias_por_tienda
@@ -9,8 +8,3 @@ from app.services.movimiento_service import get_movimiento, get_resumen_movimien
 from app.services.faltantes_service import get_faltantes
 from app.services.reabastecimiento_service import get_reabastecimiento_avanzado
 from app.services.redistribucion_service import get_redistribucion_regional
-from app.database import (get_connection, date_subtract_days, date_format_convert,
-    current_date, DB_TYPE, DATA_DIR
-)
-
-DB_NAME = "jagi_mahalo.db"
